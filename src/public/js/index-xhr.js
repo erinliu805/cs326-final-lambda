a25e7be7a379e5d904464d43c9044ace82f4df14
Erin Liu <erinliu@Erins-MacBook-Pro.local>
2020-04-24 21:27:08 -0400

    add client

---
+++ src/public/js/index-xhr.js
@@ -0,0 +1,17 @@
+const url = "http://0.0.0.0:8080/housing101";
+
+async function postData(url, data) {
+    const resp = await fetch(url,
+                             {
+                                 method: 'POST',
+                                 mode: 'cors',
+                                 cache: 'no-cache',
+                                 credentials: 'same-origin',
+                                 headers: {
+                                     'Content-Type': 'application/json'
+                                 },
+                                 redirect: 'follow',
+                                 body: JSON.stringify(data)
+                             });
+    return resp;
+}
