524b6d9997ac3497db57f0479e68d7688e2be92c
Erin Liu <erinliu@1x-nat-vl930-172-30-90-88.wireless.umass.edu>
2020-05-06 16:58:25 -0400

    modify right bar after log in

--- src/public/js/client.js
+++ src/public/js/client.js
@@ -1,30 +1,16 @@
 
-const url = "https://aqueous-dusk-44841.herokuapp.com/";
+const InfoURL = "https://aqueous-dusk-44841.herokuapp.com/get_user_info"
 
-async function postData(url, data) {
-    const resp = await fetch(url,
-                             {
-                                 method: 'POST',
-                                 mode: 'cors',
-                                 cache: 'no-cache',
-                                 credentials: 'same-origin',
-                                 headers: {
-                                     'Content-Type': 'application/json'
-                                 },
-                                 redirect: 'follow',
-                                 body: JSON.stringify(data)
-                             });
-    return resp;
+function get_right_bar(){
+	(async() =>{
+		let resp = await fetch(InfoURL)
+		let j = await resp.json()
+		if (j['result'] == 'success'){
+            let username =j['username']
+            let right_bar = document.getElementById("rightBar")
+            right_bar.innerHTML=`<div><a link="/profile">Welcome back ${username}</a><div>`
+		} else {
+			console.log("Something goes wrong")
+		}
+	})()
 }
-
-function readPost() {
-    (async () => {
-        console.log("Reading from server");
-        const newURL = listenURL + "/read";
-        const resp  = await fetch (newURL);
-        const j = await resp.json();
-        let html = generateHTML(j["author"], j['title'], j['content']);
-        console.log(html);
-        var postSection = document.getElementById("posts");
-        postSection.innerHTML = html;
-    })()};
