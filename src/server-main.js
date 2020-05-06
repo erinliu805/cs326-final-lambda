37babbc449e5d291dcbde0875cbf84f9d6d18e64
Juelin Liu <l@1x-nat-vl932-172-30-207-60.wireless.umass.edu>
2020-05-06 18:27:00 -0400

    ready to deploy

--- src/server-main.js
+++ src/server-main.js
@@ -1,9 +1,9 @@
 'use strict';
 exports.__esModule = true;
-var mongo_database_1 = require("./mongo-database");
+var mongo_database_stable_1 = require("./mongo-database-stable");
 var myserver_routing_1 = require("./myserver-routing");
 var dotenv = require('dotenv');
 dotenv.config();
-var theDatabase = new mongo_database_1.Database('housing101');
+var theDatabase = new mongo_database_stable_1.Database('housing101');
 var theServer = new myserver_routing_1.MyServer(theDatabase);
 theServer.listen(process.env.PORT);
